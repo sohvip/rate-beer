eaf2d955bba8aa9f3796fe03c266d0e37a43095c
Sohvi Puska <sohvip1@gmail.com>
2025-01-20 16:44:10 +0200

    week 2 exercises 1-4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,9 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    beer_ratings = ratings.find_all { |rating| rating.beer_id == self.id }
+    avg = beer_ratings.inject(0) { |sum, r| sum + r.score.to_f } / beer_ratings.size
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root "breweries#index"
+  get "ratings", to: "ratings#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
