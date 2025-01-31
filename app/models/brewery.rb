a0add646382f4ea98fb064039a19836fb3b1b5c8
Sohvi Puska <sohvip1@gmail.com>
2025-01-31 15:39:31 +0200

    week 3 exercises 7-8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,11 @@
 class Brewery < ApplicationRecord
   include RatingAverage
 
+  validates :name, presence: true
+  validates :year, numericality: { greater_than_or_equal_to: 1040,
+                                   only_integer: true }
+  validate :less_than_or_equal_to_current_year
+
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
@@ -13,5 +18,11 @@ class Brewery < ApplicationRecord
   def restart
     self.year = 2025
     puts "changed year to #{year}"
+  end
+
+  def less_than_or_equal_to_current_year
+    return unless year > Time.now.year
+
+    errors.add(:year, "can't be greater than current year")
   end
 end
