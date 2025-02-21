212a5a8c742bd978199d9d06866eb83bea69353d
Sohvi Puska <sohvip1@gmail.com>
2025-02-21 18:18:16 +0200

    week 5 exercises 5-6

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,13 +1,25 @@
 class PlacesController < ApplicationController
+  before_action :set_place, only: %i[show]
   def index
+  end
+
+  def show
   end
 
   def search
     @places = BeermappingApi.places_in(params[:city])
+    session[:last_search] = params[:city]
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
       render :index, status: 418
     end
+  end
+
+  private
+
+  def set_place
+    places = Rails.cache.read(session[:last_search])
+    @place = places.find { |place| place[:id] == params[:id] }
   end
 end
--- spec/services/beermapping_api_spec.rb
+++ spec/services/beermapping_api_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 describe "BeermappingApi" do
-  it "when HTTP GET returns one entry, it is parsed and returned" do
+  describe "in case of cache miss" do
+    before :each do
+      Rails.cache.clear
+    end
 
-    canned_answer = <<-END_OF_STRING
-<?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>18856</id><name>Panimoravintola Koulu</name><status>Brewpub</status><reviewlink>https://beermapping.com/location/18856</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=18856&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=18856&amp;d=1&amp;type=norm</blogmap><street>Eerikinkatu 18</street><city>Turku</city><state></state><zip>20100</zip><country>Finland</country><phone>(02) 274 5757</phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
-    END_OF_STRING
+    it "When HTTP GET returns one entry, it is parsed and returned" do
+      canned_answer = <<-END_OF_STRING
+  <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>18856</id><name>Panimoravintola Koulu</name><status>Brewpub</status><reviewlink>https://beermapping.com/location/18856</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=18856&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=18856&amp;d=1&amp;type=norm</blogmap><street>Eerikinkatu 18</street><city>Turku</city><state></state><zip>20100</zip><country>Finland</country><phone>(02) 274 5757</phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
+  END_OF_STRING
 
-    stub_request(:get, /.*turku/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+      stub_request(:get, /.*turku/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
 
-    places = BeermappingApi.places_in("turku")
+      places = BeermappingApi.places_in("turku")
 
-    expect(places.size).to eq(1)
-    place = places.first
-    expect(place.name).to eq("Panimoravintola Koulu")
-    expect(place.street).to eq("Eerikinkatu 18")
+      expect(places.size).to eq(1)
+      place = places.first
+      expect(place.name).to eq("Panimoravintola Koulu")
+      expect(place.street).to eq("Eerikinkatu 18")
+    end
   end
 
   it "when HTTP GET returns no entries, it returns an empty table" do
@@ -48,5 +53,27 @@ describe "BeermappingApi" do
     place2 = places.last
     expect(place2.name).to eq("Sonnisaari")
     expect(place2.street).to eq("Tuotekuja 1")
+  end
+
+  describe "in case of cache hit" do
+    before :each do
+      Rails.cache.clear
+    end
+
+    it "When one entry in cache, it is returned" do
+      canned_answer = <<-END_OF_STRING
+  <?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>18856</id><name>Panimoravintola Koulu</name><status>Brewpub</status><reviewlink>https://beermapping.com/location/18856</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=18856&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=18856&amp;d=1&amp;type=norm</blogmap><street>Eerikinkatu 18</street><city>Turku</city><state></state><zip>20100</zip><country>Finland</country><phone>(02) 274 5757</phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
+  END_OF_STRING
+
+      stub_request(:get, /.*turku/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+
+      BeermappingApi.places_in("turku")
+      places = BeermappingApi.places_in("turku")
+
+      expect(places.size).to eq(1)
+      place = places.first
+      expect(place.name).to eq("Panimoravintola Koulu")
+      expect(place.street).to eq("Eerikinkatu 18")
+    end
   end
 end
